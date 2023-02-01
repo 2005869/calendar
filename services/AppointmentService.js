39833a327cd8493c377deb44fd2b42c9152fcdc4
2005869 <2005869@aluno.univesp.br>
2023-02-01 17:44:00 -0300

    add factories

--- services/AppointmentService.js
+++ services/AppointmentService.js
@@ -1,11 +1,12 @@
-var appointment = require('../models/Appointment');
+const appointment = require('../models/Appointment');
 const mongoose = require('mongoose');
+const AppointmentFactory = require('../factories/AppointmentFactory');
 
 const Appointment = mongoose.model('Appointment', appointment);
 
 class AppointmentService{
     async Create(name, email, description, cpf, date, time){
-        var Appo = new Apointment({
+        var Appo = new Appointment({
             name, email, description, cpf, date, time, finished: false
         });
         try{
@@ -21,7 +22,17 @@ class AppointmentService{
         if (showFinished){
             return await Appointment.find();
         }else{
-            return await Appointment.find({finished: false});
+            var appos = await Appointment.find({finished: false});
+            var appointments = [];
+            appos.forEach(element => {
+
+                if (element.date != undefined){
+                    appointments.push(AppointmentFactory.Build(element));
+                }
+                
+            });
+
+            return appointments;
         }
     }
 }
