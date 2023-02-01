4ee3ea7ceed04dc055d8e8f84af453845c5fb679
2005869 <2005869@aluno.univesp.br>
2023-02-01 14:14:22 -0300

    initial settings

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const serverPORT = 8080;
+const mongoHost = 'localhost';
+const mongoPORT = 27017;
+const database = 'calendar';
+const optionsDatabase = {useNewUrlParser: true, useUnifiedTopology: true};
+
+// Use
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Set
+app.set('view engine', 'ejs');
+
+// connect mongodb
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb://${mongoHost}:${mongoPORT}/${database}`, optionsDatabase).then(() => {
+    console.log('Database connection successful');
+}).catch(() => {
+    console.log('Database connection error');
+});
+
+
+// Rotes
+app.get('/', (req, res) => {
+
+});
+
+
+app.listen(serverPORT, () => {
+    console.log(`Server run in port ${serverPORT}`);
+});
